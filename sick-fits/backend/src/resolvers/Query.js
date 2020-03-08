bd4a1828a6059fa12f3faa9b16d50ec5f24c265f
Tanya Powell <hello@tanyapowell.co.uk>
2020-03-08 15:30:53 +0000

    initial yoga queries and mutations

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+var {forwardTo} = require('prisma-binding');
+
+// Get requests to db go here
+const Query = {
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   console.log(ctx)
+  //   const items = await ctx.db.query.items();
+
+  //   return items;
+  // }
+};
 
 module.exports = Query;
